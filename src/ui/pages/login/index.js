9899d060ad59bc644464f45884ed08831f0ae014
royzchow <roychow123@gmail.com>
2022-07-14 18:10:36 +0800

    change initial value of loginStatus
    change initial value of loginStatus

--- src/ui/pages/login/index.js
+++ src/ui/pages/login/index.js
@@ -11,7 +11,7 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const [loginStatus, setLoginStatus] = useState(true);
+  const [loginStatus, setLoginStatus] = useState(false);
   const [userInfo, setUserInfo] = useState();
 
   const handleSubmit = () => {
